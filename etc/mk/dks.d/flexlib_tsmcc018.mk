baca64f1b5e8a116c43d2a7f005b74f0f399373d
Staf Verhaegen <staf@stafverhaegen.be>
2020-10-21 17:25:03 +0200

    Adapted to latest version of flexlib for TSMC_C018
    * Rename design flow from FlexLib018 to flexlib_tsmcc018.
    * Find flexlib liberty file relative to NDA_TOP, this removes need for
      FlexLib_CM018 environment variable.

--- etc/mk/dks.d/flexlib_tsmcc018.mk
+++ etc/mk/dks.d/flexlib_tsmcc018.mk
@@ -1,5 +1,7 @@
+ 
 
  export         REAL_MODE = Yes
  export    MBK_TARGET_LIB = 
  export      MBK_CATA_LIB = 
- export      LIBERTY_FILE = $(FlexLib_CM018)/FlexLib.lib
+ export      LIBERTY_FILE = $(NDA_TOP)/etc/coriolis2/NDA/node180/tsmc_c018/FlexLib.lib
+
